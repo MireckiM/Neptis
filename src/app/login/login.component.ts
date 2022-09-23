fb9aa17f61db88f3c606191542cb5e7fc9c2179d
Mirecki <marmir@st.amuedu.pl>
2022-09-23 14:37:35 +0200

    login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  successMsg: string = "";
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      password: ['', [Validators.required, Validators.pattern("[A-Za-z0-9@!_]{6,}")]]
+    })
+  }
+
+  login() {
+    //console.log(this.loginForm)
+    this.http.post('https://jsonplaceholder.typicode.com/posts/', this.loginForm).subscribe(responseData => { console.log(responseData) },
+      (error) => { window.alert(error) },
+      () => { window.alert("Zalogowano") }
+    )
+  }
 }
